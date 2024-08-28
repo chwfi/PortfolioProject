454eadccd0e41c43a0b33ed639df2d815e26cf0c
최원석 <98933635+chwfi@users.noreply.github.com>
2024-08-28 09:32:41 +0900

    UI 순서 변경 기능 추가
    UI 순서 변경 기능 추가

--- Assets/01.Scripts/UI/Popups/UI_Popup.cs
+++ Assets/01.Scripts/UI/Popups/UI_Popup.cs
@@ -4,38 +4,41 @@ using System.Linq;
 using UnityEngine;
 using Util;
 
-public abstract class UI_Popup : UI_Base
+public abstract class UI_Popup : UI_Base // 창 UI(PopupUI)들의 부모가 되는 클래스
 {
     [Header("UI_Fade")]
-    [SerializeField] private float _fadeDealy = 0f;
-    [SerializeField] private float _fadeTime;
+    [SerializeField] private float _fadeDealy = 0f; // UI가 켜질때 바로 켜지지 않고 딜레이를 줄것인가
+    [SerializeField] private float _fadeTime; // UI가 켜지거나 꺼질 때 몇초동안 페이드를 줄것인가
 
-    protected CanvasGroup _canvasGroup;
-    protected List<UI_Button> _buttonList = new();
+    protected CanvasGroup _canvasGroup; // 팝업 UI는 캔버스 그룹으로 관리한다
+    protected List<UI_Button> _buttonList = new(); // UI에 달려있는 버튼들의 리스트
 
 
     public virtual void Awake()
     {
-        if (TryGetComponent(out CanvasGroup canvasGroup))
+        if (TryGetComponent(out CanvasGroup canvasGroup)) // 캔버스 그룹 가져오기
             _canvasGroup = canvasGroup;
 
-        var buttons = transform.GetComponentsInChildren<UI_Button>();
+        var buttons = transform.GetComponentsInChildren<UI_Button>(); // UI에 달려있는 버튼들 가져오기
 
         if (buttons.Any()) 
         {
             _buttonList.AddRange(buttons);
         }
 
-        AccessUI(false);
+        AccessUI(false); // 시작하면 UI 꺼줌
     }
 
-    public virtual void AccessUI(bool active)
+    public virtual void AccessUI(bool active) // UI에 접근하여 키거나 끌 수 있는 함수
     {
         SetInteractive(active);
 
-        CoroutineUtil.CallWaitForSeconds(_fadeDealy, () =>
+        if (active)
+            transform.SetAsLastSibling(); // 만약 UI가 켜진다면, 자식 순서를 가장 나중으로 하여 맨 앞에 띄워지게 한다.
+
+        CoroutineUtil.CallWaitForSeconds(_fadeDealy, () => // 코루틴 유틸 클래스를 이용하여 딜레이 시간만큼 기다려주고,
         {
-            _canvasGroup.DOFade(active ? 1f : 0f, _fadeTime);
+            _canvasGroup.DOFade(active ? 1f : 0f, _fadeTime); // 캔버스 그룹을 active bool값에 따라 키거나 꺼준다. fade 가능 
         });
     }
 
